890fd0280c2747c7fc8821e5329b26955e31db9b
OLANREWAJU Oladimeji Nathaniel <olanrewajuoladimeji5@gmail.com>
2024-10-29 21:06:10 +0100

    service worker added

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import { hydrate } from 'react-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import the service worker registration
 
 hydrate(
   <BrowserRouter>
@@ -9,6 +10,9 @@ hydrate(
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+// Register the service worker
+serviceWorkerRegistration.register();
 
 if (module.hot) {
   module.hot.accept();
